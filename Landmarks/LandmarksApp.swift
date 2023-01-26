c3e3146ea848f272a3eb474aa5f7f7db5b1bec9b
Yuta Shoji <shoshoshon.music@gmail.com>
2023-01-27 07:56:23 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yuta Shoji on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
